ba938799720d51c27ece82f633421cb52437f497
benniekrijger <benniekrijger@hotmail.com>
2017-04-18 11:47:22 +0200

    Upgraded to akka 2.5, bumped version

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 name := """kafka-akka-extension"""
 
-version := "0.18"
+version := "0.19"
 
 scalaVersion := "2.12.1"
 
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,20 +2,20 @@ import sbt._
 
 object  Dependencies {
   object Version {
-    val akka = "2.4.16"
+    val akka = "2.5.0"
   }
 
   val common = Seq(
-    "com.typesafe.akka"       %% "akka-stream-kafka"          % "0.13",
+    "com.typesafe.akka"       %% "akka-stream-kafka"          % "0.14",
     "com.typesafe.akka"       %% "akka-actor"                 % Version.akka,
     "com.typesafe.akka"       %% "akka-remote"                % Version.akka,
     "com.typesafe.akka"       %% "akka-slf4j"                 % Version.akka,
     "com.iheart"              %% "ficus"                      % "1.4.0",
-    "ch.qos.logback"          %  "logback-classic"            % "1.1.8"           % "test",
-    "org.slf4j"               %  "log4j-over-slf4j"           % "1.7.12"          % "test",
+    "ch.qos.logback"          %  "logback-classic"            % "1.2.3"           % "test",
+    "org.slf4j"               %  "log4j-over-slf4j"           % "1.7.25"          % "test",
     "org.scalatest"           %% "scalatest"                  % "3.0.1"           % "test",
     "com.typesafe.akka"       %% "akka-testkit"               % Version.akka      % "test",
-    "net.manub"               %% "scalatest-embedded-kafka"   % "0.11.0"          % "test" exclude("log4j", "log4j")
+    "net.manub"               %% "scalatest-embedded-kafka"   % "0.12.0"          % "test" exclude("log4j", "log4j")
   )
 
 }
