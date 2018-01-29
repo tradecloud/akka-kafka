5bc661729972de71b6ae04643716c7085a622228
benniekrijger <benniekrijger@hotmail.com>
2018-01-29 20:08:42 +0100

    upgrade deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,13 +6,13 @@ object  Dependencies {
   }
 
   val common = Seq(
-    "com.typesafe.akka"       %% "akka-stream-kafka"          % "0.18",
+    "com.typesafe.akka"       %% "akka-stream-kafka"          % "0.19",
     "com.typesafe.akka"       %% "akka-actor"                 % Version.akka,
     "com.typesafe.akka"       %% "akka-remote"                % Version.akka,
     "com.typesafe.akka"       %% "akka-slf4j"                 % Version.akka,
     "ch.qos.logback"          %  "logback-classic"            % "1.2.3"           % "test",
     "org.slf4j"               %  "log4j-over-slf4j"           % "1.7.25"          % "test",
-    "org.scalatest"           %% "scalatest"                  % "3.0.4"           % "test",
+    "org.scalatest"           %% "scalatest"                  % "3.0.5"           % "test",
     "com.typesafe.akka"       %% "akka-testkit"               % Version.akka      % "test",
     "net.manub"               %% "scalatest-embedded-kafka"   % "1.0.0"           % "test" exclude("log4j", "log4j")
   )
